2947198fe7b4972185c7a3b077e34955315a6d97
Tomo Masakura <tomo.masakura@gmail.com>
2025-12-31 08:31:50 +0900

    Koa サーバーを追加し、`start` スクリプトと関連ライブラリを構成

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Happy developing ✨')
+import Koa from 'koa';
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello, world!';
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
